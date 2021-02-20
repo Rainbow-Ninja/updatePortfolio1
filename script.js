be1d154af2fe6964ad0c838f900c90f6f4580997
Jo Dunham <scaffelpike@yahoo.com.au>
2021-02-21 04:22:43 +1100

    styling contacts page

--- script.js
+++ script.js
@@ -6,17 +6,13 @@ const projectPage = document.querySelector('#projects');
 const contactPage = document.querySelector('#contact');
 
 open.addEventListener('click', () => {
-    homePage.classList.add('show-nav');
-    aboutPage.classList.add('show-nav');
-    projectPage.classList.add('show-nav');
-    contactPage.classList.add('show-nav');
+    const sectOpen = document.querySelectorAll("section");
+    sectOpen.forEach(el => el.classList.add('show-nav'));
 })
-// close.addEventListener('click', () => container.classList.remove('show-nav'));
+
 close.addEventListener('click', () => {
-    homePage.classList.remove('show-nav');
-    aboutPage.classList.remove('show-nav');
-    projectPage.classList.remove('show-nav');
-    contactPage.classList.remove('show-nav');
+    const sectClose = document.querySelectorAll("section");
+    sectClose.forEach(el => el.classList.remove('show-nav'));
 });
 
 
@@ -24,56 +20,70 @@ function focusHome(){
     const sectIdx = document.querySelectorAll("section");
     sectIdx.forEach(el => {
         el.style.zIndex = -1;
+        el.style.opacity = 0;
         el.classList.remove('show-nav');
     });
-    document.getElementById('home').style.zIndex=1;
-    document.querySelector('.circle-container').style.zIndex=100;
-
+    document.getElementById('home').setAttribute(
+        "style", "zIndex: 100; opacity: 1;");
 }
 
 function focusAbout(){
     const sectIdx = document.querySelectorAll("section");
     sectIdx.forEach(el => {
         el.style.zIndex = -1;
+        el.style.opacity = 0;
         el.classList.remove('show-nav');
     });
-    document.getElementById('about').style.zIndex=1;
+    document.getElementById('about').setAttribute(
+        "style", "zIndex: 100; opacity: 1;");
+}
 
-    document.querySelector('.circle-container').style.zIndex=100;
-    homePage.classList.remove('show-nav');
+function focusEd(){
+    const sectIdx = document.querySelectorAll("section");
+    sectIdx.forEach(el => {
+        el.style.zIndex = -1;
+        el.style.opacity = 0;
+        el.classList.remove('show-nav');
+    });
+    document.getElementById('edu').setAttribute(
+        "style", "zIndex: 100; opacity: 1;");
 }
 
 function focusProjects(){
     const sectIdx = document.querySelectorAll("section");
     sectIdx.forEach(el => {
         el.style.zIndex = -1;
+        el.style.opacity = 0;
         el.classList.remove('show-nav');
     });
-    document.getElementById('projects').style.zIndex=1;
-    document.querySelector('.circle-container').style.zIndex=100;
-
-    // homePage.classList.remove('show-nav');
+    document.getElementById('projects').setAttribute(
+        "style", "zIndex: 100; opacity: 1;");
 }
 
 function focusContact(){
-    // document.getElementById('home').setAttribute(
-    //     "style", "zIndex: -1; opacity: 0;"
-    // );
-    // document.getElementById('about').setAttribute(
-    //     "style", "zIndex: -1; opacity: 0;"
-    // );
-    // document.getElementById('projects').setAttribute(
-    //     "style", "zIndex: -1; opacity: 0;"
-    // );
-    // document.getElementById('contact').setAttribute(
-    //     "style", "zIndex: 10; opacity: 1;"
-    // );
     const sectIdx = document.querySelectorAll("section");
     sectIdx.forEach(el => {
         el.style.zIndex = -1;
+        el.style.opacity = 0;
         el.classList.remove('show-nav');
     });
-    document.getElementById('contact').style.zIndex=1;
-    document.querySelector('.circle-container').style.zIndex=100;
-    homePage.classList.remove('show-nav');
+    document.getElementById('contact').setAttribute(
+        "style", "zIndex: 100; opacity: 1;");
+}
+
+
+// expanding cards
+const panels = document.querySelectorAll('.panel')
+
+panels.forEach(panel => {
+    panel.addEventListener('click', () => {
+        removeActiveClasses();
+        panel.classList.add('active');
+    })
+})
+
+function removeActiveClasses() {
+    panels.forEach(panel => {
+        panel.classList.remove('active');
+    })
 }
