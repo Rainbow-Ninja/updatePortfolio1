2cec3dbf44eb91e65ea8be227931b953c1896bd5
Jo Dunham <scaffelpike@yahoo.com.au>
2021-02-24 21:50:13 +1100

    attempts at scroll

--- script.js
+++ script.js
@@ -22,11 +22,11 @@ function focusHome(){
         el.style.zIndex = -1;
         el.style.opacity = 0;
         // el.style.display = "none";
+        // el.style.transform = "translateX(100vw)"
         el.classList.remove('show-nav');
     });
     document.getElementById('home').setAttribute(
         "style", "zIndex: 100; opacity: 1;");
-    let blip = document.getElementById('home');
 }
 
 function focusAbout(){
@@ -35,10 +35,11 @@ function focusAbout(){
         el.style.zIndex = -1;
         el.style.opacity = 0;
         // el.style.display = "none";
+        // el.style.transform = "translateX(100vw)"
         el.classList.remove('show-nav');
     });
     document.getElementById('about').setAttribute(
-        "style", "zIndex: 100; opacity: 1;");
+        "style", "zIndex: 100; opacity: 1;")
 }
 
 function focusEd(){
@@ -97,36 +98,36 @@ function focusContact(){
 /*
   creates a closure that does not pollute global space
 */
-(function(el) {
-    // so this variable is available only within here
-    var minDeltaY;
-    // not the recommended way to add events, but the easiest
-    el.onwheel = function(e) {
-      // we want to eliminate vertical scrolling
-      if (e.deltaY) {
-        // normalize
-        if (minDeltaY > Math.abs(e.deltaY) || !minDeltaY) {
-          minDeltaY = Math.abs(e.deltaY);
-        }
-        // scroll a decent amount
-        el.scrollLeft += (e.deltaY / minDeltaY)
-          * ((el.scrollWidth - el.clientWidth) * 0.05);
-        // do not let other wheel events to fire
-        e.stopPropagation();
-        e.cancelBubble = true; // same for old IE
-        // and we don't want the default action either
-        e.preventDefault();
-        e.returnValue = false; // same for old IE
-      }
-    }
-    // support browsers that do not support DOM Level 3 wheel
-    el.onmousewheel = function(e) {
-      // old Internet Explorer
-      if (!e) e = window.event;
-      // we normalize the value so no need to guess how to convert
-      e.deltaY = -e.wheelDelta;
-      // we have done enough
-      el.onwheel(e);
-    }
-  })(document.getElementById('carousel'));
-  // ^----- we pass "el" to the above function down here!
+// (function(el) {
+//     // so this variable is available only within here
+//     var minDeltaY;
+//     // not the recommended way to add events, but the easiest
+//     el.onwheel = function(e) {
+//       // we want to eliminate vertical scrolling
+//       if (e.deltaY) {
+//         // normalize
+//         if (minDeltaY > Math.abs(e.deltaY) || !minDeltaY) {
+//           minDeltaY = Math.abs(e.deltaY);
+//         }
+//         // scroll a decent amount
+//         el.scrollLeft += (e.deltaY / minDeltaY)
+//           * ((el.scrollWidth - el.clientWidth) * 0.05);
+//         // do not let other wheel events to fire
+//         e.stopPropagation();
+//         e.cancelBubble = true; // same for old IE
+//         // and we don't want the default action either
+//         e.preventDefault();
+//         e.returnValue = false; // same for old IE
+//       }
+//     }
+//     // support browsers that do not support DOM Level 3 wheel
+//     el.onmousewheel = function(e) {
+//       // old Internet Explorer
+//       if (!e) e = window.event;
+//       // we normalize the value so no need to guess how to convert
+//       e.deltaY = -e.wheelDelta;
+//       // we have done enough
+//       el.onwheel(e);
+//     }
+//   })(document.getElementById('carousel'));
+//   // ^----- we pass "el" to the above function down here!
